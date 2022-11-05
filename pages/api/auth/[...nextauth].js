452c54a88e67f78b2bfc557a9c24814c6195e2d4
Decole mills <okpoka92@gmail.com>
2022-11-05 15:41:24 +0100

    last

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
 
-  secret: process.env.NEXTAUTH_SECRET,
+  secret:process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
